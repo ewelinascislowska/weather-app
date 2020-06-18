d3c81b6778eaf29e1f320737be0e7c307ca3b99d
fallesti <fallesti@gmail.com>
2020-06-18 10:25:53 +0200

    Updates

---
+++ precache-manifest.c550b714f64800592f311ddbd4aff7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a086dd04ef0cb61b06a345642af7e7",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c88dad8652ed487c4072",
+    "url": "/weather-app/static/css/main.7c4e20a9.chunk.css"
+  },
+  {
+    "revision": "afd95d348e1c05eb3c0e",
+    "url": "/weather-app/static/js/2.132aa6df.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/weather-app/static/js/2.132aa6df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88dad8652ed487c4072",
+    "url": "/weather-app/static/js/main.e3e7bf3f.chunk.js"
+  },
+  {
+    "revision": "2b98806e2138b649a4a6",
+    "url": "/weather-app/static/js/runtime-main.5abf65eb.js"
+  },
+  {
+    "revision": "83bda00400466da8d9f5e4e5ab1bdc88",
+    "url": "/weather-app/static/media/sky.83bda004.jpg"
+  }
+]);
